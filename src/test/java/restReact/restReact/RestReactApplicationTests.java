6b46e16f0f067dd29f118503cd75b1aef6a65ae4
Samuelvphipps <samuel.v.phipps@gmail.com>
2023-01-23 10:59:54 -0600

    init

---
+++ src/test/java/restReact/restReact/RestReactApplicationTests.java
@@ -0,0 +1,13 @@
+package restReact.restReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
